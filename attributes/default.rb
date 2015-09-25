0fbb9cf2583937863767deea5d835da8101956d9
Christoph Lukas <lukas@pingworks.de>
2015-09-25 22:23:32 +0200

    added chef-ws-base repo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['ws-git-repo']['repos'] = %w(phonebook.git)
+default['ws-git-repo']['repos'] = %w(phonebook.git chef-ws-base.git)
 default['ws-git-repo']['jenkins-url'] = "http://jkmaster:8080/jenkins"
 default['ws-git-repo']['jenkins-job'] = "1-commit-stage"
